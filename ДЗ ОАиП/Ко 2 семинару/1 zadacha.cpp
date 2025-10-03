72817c7f35354f766f0f87740b6e871fd2db31e9
sstt567 <stasyansuanov@gmail.com>
2025-10-03 09:12:49 +0300

    Add files via upload

---
+++ ДЗ ОАиП/Ко 2 семинару/1 zadacha.cpp
@@ -0,0 +1,17 @@
+/*
+#include<iostream>;
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a = 0;
+    cout << "Введите значение в сантиметрах:";
+    cin >> a;
+
+    int c = a / 100;
+    int d = a % 100;
+
+    cout << c << "m" << d << "cm" << endl;
+    return 0;
+
+}
+*/
---
+++ ДЗ ОАиП/Ко 2 семинару/4 zadacha.cpp
@@ -0,0 +1,16 @@
+/*
+#include<iostream>;
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RU");
+	int a;
+	cout << "Введите число:";
+	cin >> a;
+	if (a % 2==0) {
+		cout << "Число четное";
+	}
+	else {
+		cout << "Число нечетное";
+	}
+	return 0;
+}*/
