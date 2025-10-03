c152958147f21421e0da4eb861d357851da227c8
sstt567 <stasyansuanov@gmail.com>
2025-10-03 09:03:31 +0300

    Add files via upload

---
+++ ДЗ ОАиП/8 zadacha.cpp
@@ -0,0 +1,28 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    char c;
+    int a = 0;
+
+    cout << "Введите строку: ";
+
+    while (cin.get(c) && c != '\n') {
+
+        if (c == 'a' || c == 'A' ||
+            c == 'e' || c == 'E' ||
+            c == 'i' || c == 'I' ||
+            c == 'o' || c == 'O' ||
+            c == 'u' || c == 'U') {
+            a++;
+        }
+    }
+
+        cout << "Количество гласных букв: " << a << endl;
+  
+    return 0;
+}
+*/
